ad75085678c0d34053f79cf89cfc47d4183b0047
Norette Ingabire <noretteingabire@gmail.com>
2019-02-12 23:16:44 -0500

    function

--- functions.sql
+++ functions.sql
@@ -1,10 +1,30 @@
+DROP FUNCTION IF EXISTS getScore;
+
 DELIMITER $$
-
- DROP FUNCTION IF EXISTS ViewRevId;
-CREATE FUNCTION ViewRevId(reviewer INT) RETURNS INT
+CREATE FUNCTION getScore(manuID INT) RETURNS INT
+DETERMINISTIC
 BEGIN
-SET @rev_id = reviewer;
-RETURN rev_id;
-END; $$
 
+DECLARE result INT Default 0;
+DECLARE minScore INT DEFAULT 40;
+CREATE TEMPORARY TABLE IF NOT EXISTS output (value VARCHAR(255));
+
+SELECT (AVG(`feedback_appropr`) + AVG(`feedback_clarity`) + AVG(`feedback_method`) + AVG(`feedback_exp`))/4
+INTO result
+FROM Feedback
+WHERE Feedback.Manuscript_manu_id = manuID;
+
+IF (result < minScore) THEN
+	UPDATE Manuscript SET `manu_status` = 5
+    WHERE `manu_id` = manuID;
+else
+	UPDATE Manuscript SET `manu_status` = 4
+    WHERE `manu_id` = manuID;
+END IF;
+
+return result;
+END $$
 DELIMITER ;
+
+select * ,getScore(3) FROM Manuscript;
+SELECT * FROM Manuscript;
