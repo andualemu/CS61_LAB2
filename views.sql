430bbfac416e7a3f791027451011b5fdc3500748
Norette Ingabire <noretteingabire@gmail.com>
2019-02-12 22:25:29 -0500

    views

--- views.sql
+++ views.sql
@@ -36,7 +36,63 @@ LEFT JOIN
 Reviewer_assignment ON Manuscript.manu_id = Reviewer_assignment.Manuscript_manu_id
 WHERE Manuscript.manu_status = 2;
 
+DELIMITER $$
+DROP FUNCTION IF EXISTS ViewRevId;
+CREATE FUNCTION ViewRevId(reviewer int) RETURNS INT
+DETERMINISTIC
+BEGIN
+SET @rev_id = reviewer;
+RETURN @rev_id;
+END; $$
+DELIMITER ;
+
+
+DROP VIEW IF EXISTS ReviewStatus;
+CREATE VIEW ReviewStatus AS
+SELECT Feedback.Manuscript_manu_id AS `Manuscript ID`,
+Manuscript.manu_title AS `Manuscript Title`,
+Reviewer_assignment.assigned_date AS `Assigned date`,
+Feedback_feedback_id AS `Feedback ID`,
+Feedback.feedback_appropr AS `Appropriateness`,
+Feedback.feedback_clarity AS `Clarity`,
+Feedback.feedback_method As `Method`,
+Feedback.feedback_exp AS `Experimental results`,
+Feedback.feedback_decision AS `Decision`
+FROM Reviewer_assignment
+JOIN Feedback
+ON Feedback.feedback_id = Reviewer_assignment.Feedback_feedback_id
+JOIN Manuscript
+WHERE Feedback.Manuscript_manu_id = Manuscript.manu_id AND Reviewer_reviewer_id = viewRevId(2);
+
+
 DROP VIEW IF EXISTS WhatsLeft;
 CREATE VIEW WhatsLeft AS
 SELECT manu_id, manu_status, updated_at
 FROM Manuscript;
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS ViewRevId;
+CREATE FUNCTION ViewRevId(reviewer int) RETURNS INT
+DETERMINISTIC
+BEGIN
+SET @rev_id = reviewer;
+RETURN @rev_id;
+END; $$
+DELIMITER ;
+
+DROP VIEW IF EXISTS ReviewStatus;
+CREATE VIEW ReviewStatus AS
+SELECT Feedback.Manuscript_manu_id AS `Manuscript ID`,
+Manuscript.manu_title AS `Manuscript Title`,
+Reviewer_assignment.assigned_date AS `Assigned date`,
+Feedback_feedback_id AS `Feedback ID`,
+Feedback.feedback_appropr AS `Appropriateness`,
+Feedback.feedback_clarity AS `Clarity`,
+Feedback.feedback_method As `Method`,
+Feedback.feedback_exp AS `Experimental results`,
+Feedback.feedback_decision AS `Decision`
+FROM Reviewer_assignment
+JOIN Feedback
+ON Feedback.feedback_id = Reviewer_assignment.Feedback_feedback_id
+JOIN Manuscript
+WHERE Feedback.Manuscript_manu_id = Manuscript.manu_id AND Reviewer_reviewer_id = viewRevId(2);
